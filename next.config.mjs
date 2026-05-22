9c0dd89004620cf590fcee7f7f24935db471c212
Guus-exe <gustavo.assuncao1620@hotmail.com>
2026-05-22 20:18:39 -0300

    chore: atualiza configuracoes de ambiente e next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
 
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+        pathname: "/storage/v1/object/public/**"
+      }
+    ]
+  },
+
   async headers() {
     return [
       {
