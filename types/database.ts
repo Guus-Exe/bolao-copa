97bba5be8543d396d479ff4ffa118c8bb1cb167e
Guus-exe <gustavo.assuncao1620@hotmail.com>
2026-05-23 21:41:44 -0300

    feat: create database types file

--- types/database.ts
+++ types/database.ts
@@ -182,6 +182,8 @@ export type Database = {
           total_points: number
           total_predictions: number
           exact_scores: number
+          exact_scores_hosts: number
+          exact_scores_brazil: number
           correct_predictions: number
           position: number
         }
